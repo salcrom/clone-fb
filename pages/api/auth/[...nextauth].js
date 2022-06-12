54027bc940b1c39a5344ab267b4e0e1c46179e7a
salcrom <seya.28@gmail.com>
2022-06-12 19:56:50 +0200

    added nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,10 +7,12 @@ export default NextAuth({
     providers: [
       FacebookProvider({
         clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        nextauth_url: process.env.NEXTAUTH_URL,
-        nextauth_secret: process.env.NEXTAUTH_SECRET
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
       }),
       // ...add more providers here
     ],
+    secret: [{
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
+    }]
 })
