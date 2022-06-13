a995afcce3361a0a9eb8d197be4eb33a0ab426b6
salcrom <seya.28@gmail.com>
2022-06-13 20:37:44 +0200

    login updated

--- components/Login.js
+++ components/Login.js
@@ -14,7 +14,7 @@ export const Login = () => {
                 objectFit="contain"
             />
             <h1
-                onClick={ () => signIn('facebook', { callbackUrl: 'http://localhost:3000/api/auth/' }) }
+                onClick={ signIn }
                 className="mt-20 p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">Login with Facebook</h1>
         </div>
     )
