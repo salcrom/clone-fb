d8075226dfe79b9af51064b3f7527f0ffef803ad
salcrom <seya.28@gmail.com>
2022-06-14 13:01:04 +0200

    version #1 antes de comer

--- components/Login.js
+++ components/Login.js
@@ -14,7 +14,7 @@ export const Login = () => {
                 objectFit="contain"
             />
             <h1
-                onClick={ signIn }
+                onClick={ () => signIn('facebook', { callbackURL: 'https://clone-fb.salcromweb.com/api/auth/callback/facebook' }) }
                 className="mt-20 p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">Login with Facebook</h1>
         </div>
     )
